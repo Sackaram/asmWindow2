c5e79004df7537a5432ccc4abd7068c099a0e675
viktor hansson <vicken_92@hotmail.com>
2023-06-10 02:49:59 +0200

    Working window, its a console though..

--- test.asm
+++ test.asm
@@ -1,69 +1,80 @@
 section .data
-    ClassName db 'MyWindowClass',0
-    WindowName db 'MyWindow',0
+    ClassName db "WindowClass", 0
+    AppName db "My Window", 0
 
-    ; Define constants
-    WS_OVERLAPPEDWINDOW equ 0CF0000h
-    WS_EX_OVERLAPPEDWINDOW equ 300h
-    COLOR_WINDOW equ 5h
-    CS_VREDRAW equ 1h
-    CS_HREDRAW equ 2h
+section .bss
+    wc resb 48
+    msg resb 28
+    hwnd resd 1
 
 section .text
     global _start
+    extern _GetModuleHandleA@4, _ExitProcess@4
+    extern _RegisterClassExA@4, _CreateWindowExA@48
+    extern _ShowWindow@8, _GetMessageA@16
+    extern _TranslateMessage@4, _DispatchMessageA@4
 
-    extern _RegisterClassExA@4
-    extern _CreateWindowExA@48
-    extern _ShowWindow@8
-    extern _GetMessageA@16
-    extern _TranslateMessage@4
-    extern _DispatchMessageA@4
+_start:
+    ; Initialize WNDCLASSEX structure
+    mov dword [wc], 48 ; size of WNDCLASSEX
+    mov dword [wc+4], 3 ; CS_HREDRAW | CS_VREDRAW
+    mov dword [wc+8], WndProc
+    mov dword [wc+12], 0 ; cbClsExtra
+    mov dword [wc+16], 0 ; cbWndExtra
+    push 0 ; NULL
+    call _GetModuleHandleA@4
+    mov dword [wc+20], eax ; hInstance
+    mov dword [wc+24], 0 ; hIcon
+    mov dword [wc+28], 0 ; hCursor
+    mov dword [wc+32], 5 ; COLOR_WINDOW
+    mov dword [wc+36], 0 ; lpszMenuName
+    mov dword [wc+40], ClassName
+    mov dword [wc+44], 0 ; hIconSm
+    push wc
+    call _RegisterClassExA@4
 
-    _start:
+    ; Create Window
+    push 0 ; lParam
+    push 0 ; hMenu
+    push 0 ; hWndParent
+    push 300 ; height
+    push 300 ; width
+    push 0 ; y
+    push 0 ; x
+    push 0x80000000 ; WS_VISIBLE
+    push AppName
+    push ClassName
+    push 0 ; dwExStyle
+    call _GetModuleHandleA@4
+    push eax ; hInstance
+    call _CreateWindowExA@48
+    mov [hwnd], eax
 
-    ; Define a window class
-    push 0                    ; cbSize
-    push CS_VREDRAW | CS_HREDRAW ; style
-    call WndProc              ; lpfnWndProc
-    add esp, 4
-    push 0                    ; cbClsExtra
-    push 0                    ; cbWndExtra
-    push 0                    ; hInstance
-    push COLOR_WINDOW + 1     ; hbrBackground
-    push 0                    ; lpszMenuName
-    push ClassName            ; lpszClassName
-    call [_RegisterClassExA@4]
+    ; Show Window
+    push 1 ; nCmdShow
+    push eax ; hWnd
+    call _ShowWindow@8
 
-    ; Create a window
-    push 0                    ; lParam
-    push 0                    ; hMenu
-    push 0                    ; hWndParent
-    push 300                  ; Height
-    push 400                  ; Width
-    push 100                  ; Y
-    push 100                  ; X
-    push WS_OVERLAPPEDWINDOW  ; Style
-    push ClassName            ; lpClassName
-    push WindowName           ; lpWindowName
-    push WS_EX_OVERLAPPEDWINDOW ; dwExStyle
-    call [_CreateWindowExA@48]
+    ; Message Loop
+message_loop:
+    push 0 ; filterMax
+    push 0 ; filterMin
+    push 0 ; hWnd
+    push msg
+    call _GetMessageA@16
+    test eax, eax
+    jz end_program
+    push msg
+    call _TranslateMessage@4
+    push msg
+    call _DispatchMessageA@4
+    jmp message_loop
 
-    ; Display the window
-    push 0                    ; uCmdShow
-    call [_ShowWindow@8]
+end_program:
+    push 0
+    call _ExitProcess@4
 
-    ; Run the message loop
-    MessageLoop:
-    sub esp, 16               ; Allocate space for a MSG structure
-    push esp                  ; lpMsg
-    call [_GetMessageA@16]
+; Window Procedure
+WndProc:
+    ret 16
 
-    push esp                  ; lpMsg
-    call [_TranslateMessage@4]
-
-    push esp                  ; lpMsg
-    call [_DispatchMessageA@4]
-    jmp MessageLoop
-
-    WndProc:
-    
